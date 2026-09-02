b0a962b932f36c3659f9c3aac5dcf52228e3a9f2
Karla Paniagua <113156741+Karladanielap@users.noreply.github.com>
2026-09-01 21:34:08 -0400

    Update augment.py

--- augment.py
+++ augment.py
@@ -135,7 +135,7 @@ def cal_weight_matrix(
       adata.obsm["morphological_similarity"] = morphological_similarity	
     adata.obsm["weights_matrix_all"] = (physical_distance*gene_correlation*morphological_similarity*cellcompo_correlation)
     if no_morphological:
-      adata.obsm["weights_matrix_nomd"] = (gene_correlation*physical_distance*cellcompo_correlation)	
+      adata.obsm["weights_matrix_nomd"] = (gene_correlation*physical_distance)	
     print("The weight result of image feature is added to adata.obsm['weights_matrix_all'] !")						
   else:
     adata.obsm["weights_matrix_nomd"] = (gene_correlation* physical_distance*cellcompo_correlation)
